45696a344778af60074a5bc363faf94816bb22d9
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-04-05 09:58:01 +0800

    Create routes/getup/index.js

---
+++ routes/getup/index.js
@@ -0,0 +1,11 @@
+import fs from 'fs/promises';
+export default async function (fastify, opts) {
+	const path = 'apps/getup/records.json';
+	fastify.post('/', async function (request, reply) {
+		const item = request.body;
+		const itemArr = await fs.readFile(path).then(JSON.parse);
+		itemArr.push(item);
+		await fs.writeFile(path, JSON.stringify(itemArr, null, '	'));
+		return JSON.stringify(item);
+	})
+}
