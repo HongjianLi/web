3ba97edfd777d4d27260281ec3a461566b8ee8a0
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-05-09 10:43:22 +0800

    Append .catch(() => []) to fs.readFile().then(JSON.parse) to initialize a new json file.

--- routes/getup/index.js
+++ routes/getup/index.js
@@ -3,7 +3,7 @@ export default async function (fastify, opts) {
 	const path = 'apps/getup/records.json';
 	fastify.post('/', async function (request, reply) {
 		const item = request.body;
-		const itemArr = await fs.readFile(path).then(JSON.parse);
+		const itemArr = await fs.readFile(path).then(JSON.parse).catch(() => []);
 		itemArr.push(item);
 		await fs.writeFile(path, JSON.stringify(itemArr, null, '	'));
 		return JSON.stringify(item);
