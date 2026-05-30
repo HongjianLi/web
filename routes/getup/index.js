142db7419f773b9f717c49f6b1db60878ba0c978
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-05-30 17:53:39 +0800

    Split getup/records.json into getup/{0,1}/records.txt

--- routes/getup/index.js
+++ routes/getup/index.js
@@ -1,11 +1,8 @@
 import fs from 'fs/promises';
 export default async function (fastify, opts) {
-	const path = 'apps/getup/records.json';
 	fastify.post('/', async function (request, reply) {
-		const item = request.body;
-		const itemArr = await fs.readFile(path).then(JSON.parse).catch(() => []);
-		itemArr.unshift(item);
-		await fs.writeFile(path, JSON.stringify(itemArr, null, '	'));
-		return JSON.stringify(item);
+		const record = request.body;
+		await fs.appendFile(`apps/getup/${record.who}/records.txt`, `${record.date}\n`);
+		return JSON.stringify(record);
 	})
 }
